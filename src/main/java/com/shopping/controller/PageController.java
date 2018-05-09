f5d6a2f018671d3806781443091e1f96b9f397bc
Tural Samadov <turalsemedov1903@gmail.com>
2018-05-09 23:27:46 +0400

    css ve js fayllar elave edildi

--- src/main/java/com/shopping/controller/PageController.java
+++ src/main/java/com/shopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.shopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,9 +11,9 @@ public class PageController {
 
     @RequestMapping(value = {"/","/home","/index"})
     public ModelAndView index(){
-
         ModelAndView mv = new ModelAndView("page");
         mv.addObject("greeting", "Welcome to Spring Web MVC");
         return mv;
     }
+
 }
